55100ad860b7b143be9e08dec02f7d0b4a62c43c
Jakub Ruzicka <jruzicka@redhat.com>
2013-07-08 15:22:05 +0200

    Remove runtime dependency on python-pbr.

--- keystoneclient/__init__.py
+++ keystoneclient/__init__.py
@@ -13,10 +13,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import pbr.version
-
-
-__version__ = pbr.version.VersionInfo('python-keystoneclient').version_string()
+__version__ = "REDHATKEYSTONECLIENTVERSION"
 
 __all__ = [
     # Modules
